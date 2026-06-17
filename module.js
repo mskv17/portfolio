08c52581411608051e747bea1344579ac7cb35db
Muhammed Sinan K V <sinansanu97720@gmail.com>
2026-06-17 11:29:52 +0530

    Update module.js

--- module.js
+++ module.js
@@ -1,4 +1,4 @@
-import Analytics from "https://unpkg.com/securoanalytics-sdk@1.5.1/src/index.js";
+import Analytics from "https://unpkg.com/securoanalytics-sdk@1.5.2/src/index.js";
 
 Analytics.init({apiKey: "9324fe9d-c0cc-4595-8b61-7666fa193520"});
 
